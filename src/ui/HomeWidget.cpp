a344da502e6ceee94f55b368d82e2244e4a229f4
vadniks <whatever@fake.some>
2023-04-20 00:24:58 +0300

    UI

--- src/ui/HomeWidget.cpp
+++ src/ui/HomeWidget.cpp
@@ -1,10 +1,20 @@
 
 #include "HomeWidget.hpp"
+//#include <QtSvgWidgets/QSvgWidget>
+#include <QFile>
+
+static QByteArray a() {
+    QFile file(":/pc_icon.svg");
+    file.open(QIODevice::ReadOnly);
+    auto b = file.readAll();
+    file.close();
+    return b; // TODO
+}
 
 HomeWidget::HomeWidget(QWidget* parent) :
     QWidget(parent),
     mBaseLayout(this),
-    mAppBar(this, u8"Title", { new QPushButton(u8"Button") }),
+    mAppBar(this, u8"Title", { new QPushButton(QIcon(QPixmap::fromImage(QImage::fromData(a()))), u8"") }),
     mListView(this)
 {
     mBaseLayout.addWidget(&mAppBar);
