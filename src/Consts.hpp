da9b1f8b020a7a13a23f103aa1388c27a9cc58db
vadniks <whatever@fake.some>
2023-04-25 13:11:07 +0300

    Structure, UI & Logic

--- src/Consts.hpp
+++ src/Consts.hpp
@@ -1,40 +1,40 @@
 
 #pragma once
 
-#define U8_STRING static const char8_t*
-#define INT static const int
+#define U8_STRING(n, s) inline static const char8_t* n = u8 ## #s;
+#define INT(n, i) inline static const int n = i;
 
 class Consts final {
 private:
     inline Consts() { throw -1; } // NOLINT(hicpp-exception-baseclass)
 public:
-    U8_STRING APP_NAME;
-    U8_STRING TITLE;
-    U8_STRING DESCRIPTION;
-    U8_STRING NOT_SELECTED;
-    U8_STRING COMPONENT_SELECTION;
+    U8_STRING(APP_NAME, Componenta)
+    U8_STRING(TITLE, Title)
+    U8_STRING(DESCRIPTION, Description)
+    U8_STRING(NOT_SELECTED, Not selected)
+    U8_STRING(COMPONENT_SELECTION, Component selection)
 
-    U8_STRING DOLLAR_SIGN_WITH_ARG;
+    U8_STRING(DOLLAR_SIGN_WITH_ARG, $%1)
 
-    U8_STRING PC_ICON;
-    U8_STRING CPU_ICON;
-    U8_STRING MB_ICON;
-    U8_STRING GPU_ICON;
-    U8_STRING RAM_ICON;
-    U8_STRING HDD_ICON;
-    U8_STRING SSD_ICON;
-    U8_STRING PSU_ICON;
-    U8_STRING FAN_ICON;
-    U8_STRING CASE_ICON;
-    U8_STRING INFO_ICON;
-    U8_STRING LOGIN_ICON;
-    U8_STRING BACK_ICON;
-    U8_STRING DONE_ICON;
-    U8_STRING REMOVE_ICON;
+    U8_STRING(PC_ICON, :/pc_icon.svg)
+    U8_STRING(CPU_ICON, :/pc_cpu.svg)
+    U8_STRING(MB_ICON, :/pc_mb.svg)
+    U8_STRING(GPU_ICON, :/pc_gpu.svg)
+    U8_STRING(RAM_ICON, :/pc_ram.svg)
+    U8_STRING(HDD_ICON, :/pc_hdd.svg)
+    U8_STRING(SSD_ICON, :/pc_ssd.svg)
+    U8_STRING(PSU_ICON, :/pc_psu.svg)
+    U8_STRING(FAN_ICON, :/pc_fan.svg)
+    U8_STRING(CASE_ICON, :/pc_case.svg)
+    U8_STRING(INFO_ICON, :/info.svg)
+    U8_STRING(LOGIN_ICON, :/login.svg)
+    U8_STRING(BACK_ICON, :/back.svg)
+    U8_STRING(DONE_ICON, :/done.svg)
+    U8_STRING(REMOVE_ICON, :/remove.svg)
 
-    INT APP_WIDTH;
-    INT APP_HEIGHT;
-    INT ICON_SIZE;
+    INT(APP_WIDTH, 640)
+    INT(APP_HEIGHT, 640)
+    INT(ICON_SIZE, 30)
 };
 
 #undef U8_STRING
