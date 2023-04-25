f8f0def60ca2e8bd46aee6137598cf344517c7b1
vadniks <whatever@fake.some>
2023-04-25 23:28:46 +0300

    Structure, UI & Logic

--- src/ui/AboutWidget.cpp
+++ src/ui/AboutWidget.cpp
@@ -16,13 +16,19 @@
     mAbout(Consts::ABOUT_TEXT),
     mCopyright(Consts::COPYRIGHT)
 {
-    mImages.addWidget(&mHardware);
-    mImages.addWidget(&mQuality);
+    mAbout.setWordWrap(true);
+
+    mImages.addStretch();
+    mImages.addWidget(&mHardware, 0, Qt::AlignCenter);
+    mImages.addWidget(&mQuality, 0, Qt::AlignCenter);
+    mImages.addStretch();
 
     mBody.addWidget(&mAppBar, 0, Qt::AlignTop);
     mBody.addWidget(&mAbout);
+    mBody.addStretch();
     mBody.addLayout(&mImages);
-    mBody.addWidget(&mCopyright, 0, Qt::AlignBottom);
+    mBody.addStretch();
+    mBody.addWidget(&mCopyright, 0, Qt::AlignBottom | Qt::AlignHCenter);
 
     resizeEvent(nullptr);
 }
@@ -32,5 +38,5 @@ void AboutWidget::resizeEvent(QResizeEvent* event) {
     const auto size = static_cast<int>(static_cast<float>(Util::min(width(), height())) * 0.2);
 
     mHardware.setPixmap(QIcon(Consts::HARDWARE_ICON).pixmap(size));
-    mQuality.setPixmap(QIcon(Consts::QUALITY_ICON).pixmap(size));
+    mQuality.setPixmap(QIcon(Consts::QUALITY_ICON).pixmap(static_cast<int>(static_cast<float>(size) * 1.5)));
 }
