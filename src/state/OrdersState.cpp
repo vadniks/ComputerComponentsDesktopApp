064b23e618f526788f50cb619e2ebd2c0df00f06
vadniks <whatever@fake.some>
2023-05-11 00:42:35 +0300

    Structure, UI & Logic

--- src/state/OrdersState.cpp
+++ src/state/OrdersState.cpp
@@ -1,2 +1,26 @@
 
+#include <QtConcurrent/QtConcurrent>
 #include "OrdersState.hpp"
+
+OrdersState::OrdersState() {
+
+}
+
+QFuture<bool> OrdersState::submitOrder(
+    const QString& firstName,
+    const QString& lastName,
+    const QString& phoneNumber,
+    const QString& address
+) {
+    return QtConcurrent::run([](){ return false; });
+}
+
+QFuture<bool> OrdersState::clearHistory() {
+    return QtConcurrent::run([](){ return false; });
+}
+
+QFuture<QList<Component*>* _Nullable> OrdersState::fetchHistory() {
+    return QtConcurrent::run([](){ return new QList<Component*>(); });
+}
+
+OrdersState::~OrdersState() { for (auto component : mBoughtComponents) delete component; }
--- src/state/OrdersState.hpp
+++ src/state/OrdersState.hpp
@@ -1,6 +1,25 @@
 
 #pragma once
 
-class OrdersState {
+#include <QFuture>
+#include <QList>
+#include "../model/Component.hpp"
 
+class OrdersState final {
+public:
+    OrdersState();
+    ~OrdersState();
+
+    QFuture<bool> submitOrder(
+        const QString& firstName,
+        const QString& lastName,
+        const QString& phoneNumber,
+        const QString& address
+    );
+
+    QFuture<bool> clearHistory();
+private:
+    [[nodiscard]] QFuture<QList<Component*>* _Nullable> fetchHistory();
+
+    QList<Component*> mBoughtComponents;
 };
