2d224afd70ef2e17dbb369a160699b18e6c1e93b
vadniks <whatever@fake.some>
2023-05-11 03:55:05 +0300

    Structure, UI & Logic

--- src/ui/MainWindow.hpp
+++ src/ui/MainWindow.hpp
@@ -5,7 +5,13 @@
 #include "../state/AppState.hpp"
 #include "MessageDisplayableWidget.hpp"
 
-class MainWindow final : public QMainWindow {
+class IWindowShared {
+public:
+    enum Widget { HOME, SELECT, ABOUT, LOGIN, ORDERS };
+    [[nodiscard]] virtual Widget currentWidget() const = 0; // TODO: supply this dependency to all widgets, each widget should check current widget, if it's not instance of checker's class then it should prevent/abort all async actions as it's been detached & deallocated
+};
+
+class MainWindow final : public QMainWindow, public IWindowShared {
     Q_OBJECT
 public:
     MainWindow();
